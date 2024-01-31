fef68e0ba44cde568c96374464427bbb70f9be2c
TDHster <43290128+TDHster@users.noreply.github.com>
2024-01-31 23:25:41 +0400

    Update vm_resources_usage.sh
    Now show dynamic of process with printing dot on each vm

--- vm_resources_usage.sh
+++ vm_resources_usage.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#
 
 total_memory=0
 total_cores=0
@@ -7,7 +6,7 @@ total_cores=0
 all_available_memory_gb=$(free -m | awk '/^Mem:/ {printf "%.2f", $2/1024}')
 all_available_cores=$(nproc)
 
-echo "Start counting resources of running virtual machines..."
+echo -n "Start counting resources of running virtual machines."
 
 # Iterate over VMIDs
 for vmid in $(qm list | grep -v stopped | awk '$1 ~ /^[0-9]+$/ {print $1}'); do
@@ -16,9 +15,12 @@ for vmid in $(qm list | grep -v stopped | awk '$1 ~ /^[0-9]+$/ {print $1}'); do
 
     total_memory=$((total_memory + memory))
     total_cores=$((total_cores + cores))
+
+    echo -n "."
 done
 
 total_memory_gb=$(printf "%.2f" $(echo "$total_memory/1024" | bc -l))
 
+echo
 echo "Memory used by VMs: ${total_memory_gb}GB of ${all_available_memory_gb}GB"
 echo "Cores used by VMs: $total_cores of $all_available_cores available."
